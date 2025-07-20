cacf813baa4c474b7268ee776be9009347c5ccb3
Matthew Casperson <matthewcasperson@gmail.com>
2025-07-21 08:13:09 +1000

    Version code now workds

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
-import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
-
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Main {
     public static void main(final String[] args) throws UnknownHostException {
-        final KubernetesMockServer server = new KubernetesMockServer();
-        final InetAddress address = InetAddress.getByName("localhost");
-        server.init(address, 48433);
+       new Server().start();
     }
 }
---
+++ src/main/java/Server.java
@@ -0,0 +1,39 @@
+import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Server {
+    public Server() {
+
+    }
+
+    public void start() throws UnknownHostException {
+        final KubernetesMockServer server = new KubernetesMockServer();
+        addVersionEndpoint(server);
+        final InetAddress address = InetAddress.getByName("localhost");
+        server.init(address, 48433);
+    }
+
+    private void addVersionEndpoint(KubernetesMockServer server) {
+        final String response = """
+        {
+            "major": "1",
+            "minor": "31",
+            "gitVersion": "v1.31.0",
+            "gitCommit": "9edcffcde5595e8a5b1a35f88c421764e575afce",
+            "gitTreeState": "clean",
+            "buildDate": "2024-08-13T22:44:37Z",
+            "goVersion": "go1.22.5",
+            "compiler": "gc",
+            "platform": "linux/amd64"
+        }
+        """;
+
+        server.expect()
+                .get()
+                .withPath("/version?timeout=32s")
+                .andReturn(200, response.stripIndent())
+                .always();
+    }
+}
